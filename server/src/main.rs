d4dabb4b715d26d238087f7fe9b2868b3e8552d6
panicbit <panicbit.dev@gmail.com>
2018-07-26 03:49:56 +0200

    Add cli to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,20 @@
+#[macro_use] extern crate structopt_derive;
 use actix_web::{App, ws};
 use actix_web::actix::*;
 use boggle_server::{Server, Client};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(short = "h", long = "host", default_value = "localhost")]
+    host: String,
+    #[structopt(short = "p", long = "port", default_value = "8001")]
+    port: u16,
+}
 
 fn main() {
+    let opt = Opt::from_args();
+    let addr = (&*opt.host, opt.port);
     let system = System::new("game");
     let server = Server::new().start();
 
@@ -13,8 +25,10 @@ fn main() {
                 r.f(move |req| ws::start(req, Client::new(server.clone())))
             })
     })
-    .bind("0:8001").unwrap()
+    .bind(addr).unwrap()
     .start();
+
+    println!("Listening on {}:{}", opt.host, opt.port);
 
     system.run();
 }
