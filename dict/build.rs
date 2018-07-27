163ee093cc959d8f2208c29314370e4032f45fbf
panicbit <panicbit.dev@gmail.com>
2018-07-27 14:59:17 +0200

    Vendor dictionary
    Closes #8

--- dict/build.rs
+++ dict/build.rs
@@ -9,10 +9,10 @@ fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     let mut dict = Dict::new();
-    let words = include_str!("/usr/share/dict/british");
+    let words = include_str!("dict");
 
     for word in words.split_whitespace() {
-        dict.add(word)
+        dict.add(word);
     }
 
     let mut dict_file = File::create(out_dir.join("dict")).unwrap();
--- tools/solve/src/main.rs
+++ tools/solve/src/main.rs
@@ -1,19 +1,15 @@
 extern crate boggle;
+extern crate dict;
 
 use std::env::args;
-use boggle::{Dict, Grid};
+use boggle::Grid;
+
+use dict::DICT;
 
 fn main() {
     let grid_description = args().nth(1).expect("grid description missing");
     let grid = grid_description.parse::<Grid>().unwrap();
-    let mut dict = Dict::new();
-    let words = include_str!("/usr/share/dict/british").split_whitespace();
-
-    for word in words {
-        dict.add(word);
-    }
-
-    let words = grid.words(&dict);
+    let words = grid.words(&DICT);
 
     for word in words {
         println!("{}", word);
